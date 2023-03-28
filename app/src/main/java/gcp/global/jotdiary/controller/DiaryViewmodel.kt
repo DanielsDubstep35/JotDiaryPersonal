3a56a566c5d121013fbf081d11524b60d193dc76
danielmendes <danielmendes2@icloud.com>
2023-03-28 19:49:55 +0300

    Updated DiaryViewmodel
    Co-authored-by: Sriisky <saraegan13@gmail.com>
    Co-authored-by: DanielsDubstep35 <danielmendes2@icloud.com>

--- app/src/main/java/gcp/global/jotdiary/controller/DiaryViewmodel.kt
+++ app/src/main/java/gcp/global/jotdiary/controller/DiaryViewmodel.kt
@@ -10,41 +10,85 @@ import com.google.firebase.auth.FirebaseUser
 import gcp.global.jotdiary.model.models.Diaries
 import gcp.global.jotdiary.model.repository.StorageRepository
 
+/**
+ * This class is a type of ViewModel that is used to get the data of a single diary from
+ * the StorageRepository and display it on the screen. This can then be updated and
+ * deleted, or you could even add a new diary
+ *
+ **/
 class DiaryViewmodel (
     private val repository: StorageRepository = StorageRepository(),
 ): ViewModel() {
+
+    /** This variable gets the current state of the Diary Ui, aswell as sets the state of the Diary Ui*/
     var diaryUiState by mutableStateOf(DiaryUiState())
         private set
 
+    /** This private variable checks if there is a user logged in */
     private val hasUser:Boolean
         get() = repository.hasUser()
 
+    /** This private variable gets the current user */
     private val user: FirebaseUser?
         get() = repository.user()
 
+
+    /**
+     * This method takes in a diary title, and passes it to the Ui State
+     *
+     * @param title - This is the new title for the diary
+     */
     fun onTitleChange(title:String){
         diaryUiState = diaryUiState.copy(title = title)
     }
 
+    /**
+     * This method takes in a diary description, and passes it to the Ui State
+     *
+     * @param description - This is the new description for the diary
+     */
     fun onDescriptionChange(description:String){
         diaryUiState = diaryUiState.copy(description = description)
     }
 
+    /**
+     * This method takes in a diary date, and passes it to the Ui State
+     *
+     * @param date - This is the new date for the diary
+     */
     fun onDateChange(date:Timestamp){
         diaryUiState = diaryUiState.copy(createdDate = date)
     }
 
+    /**
+     * This method takes in a diary image, and passes it to the Ui State
+     *
+     * @param imageUri - This is the new image for the diary. This image
+     * needs to come from the users device.
+     */
     fun onImageChange(imageUri: Uri?){
         diaryUiState = diaryUiState.copy(imageUri = imageUri)
     }
 
+    /**
+     * This method takes in a diary image, and passes it to the UiState
+     *
+     * @param imageUrl - This is the new image for the diary. This image
+     * comes from a url on the internet, and is used as a default image incase
+     * no image is selected from the users device.
+     */
     fun onImageChangeUrl(imageUrl: String){
         diaryUiState = diaryUiState.copy(imageUrl = imageUrl)
     }
 
+    /**
+     * This method adds a new diary to the database if there is a user logged in.
+     * When the diary is added, a boolean is returned, depending on whether
+     * the diary is added or not
+     *
+     */
     fun addDiary(){
         if(hasUser){
-
             repository.addDiary(
                 userId = user!!.uid,
                 title = diaryUiState.title,
@@ -57,6 +101,13 @@ class DiaryViewmodel (
         }
     }
 
+    /**
+     * This method adds a new diary to the database if there is a user logged in.
+     * When the diary is added, a boolean is returned, depending on whether
+     * the diary is added or not.
+     *
+     * This method is only triggered if there is no selected image from the users device.
+     */
     fun addDiaryUrl() {
         if(hasUser) {
             repository.addDiaryUrl(
@@ -71,6 +122,14 @@ class DiaryViewmodel (
         }
     }
 
+    /**
+     * This method adds the user inputted diary data to the Ui State, which can be added
+     * to the database.
+     *
+     * @param diary - This is the new diary the user inputted, that will be used to either
+     * add a new diary, or update an existing diary by replacing the diary in question with
+     * the user inputted diary.
+     */
     fun setDiaryFields(diary: Diaries){
 
         diaryUiState = diaryUiState.copy(
@@ -81,6 +140,11 @@ class DiaryViewmodel (
         )
     }
 
+    /**
+     * This method gets a diary from the database that the user selected
+     *
+     * @param diaryId - unique for every user agent
+     */
     fun getDiary(diaryId: String) {
         repository.getDiary(
             diaryId = diaryId,
@@ -92,6 +156,11 @@ class DiaryViewmodel (
 
     }
 
+    /**
+     * This method updates the currently selected diary in the database
+     *
+     * @param diaryId - unique for every user agent
+     */
     fun updateDiary(
         diaryId: String
     ) {
@@ -107,6 +176,10 @@ class DiaryViewmodel (
         }
     }
 
+    /**
+     * This method is usually triggered if a diary is successfully added or updated. It allows
+     * new diaries to be added, or existing diaries to be updated.
+     */
     fun resetDiaryAddedStatus(){
         diaryUiState = diaryUiState.copy(
             diaryAddedStatus = false,
@@ -114,12 +187,21 @@ class DiaryViewmodel (
         )
     }
 
+    /**
+     * This method resets the state of the data class "Diary Ui State", which is useful for
+     * when the user is not viewing a diary. Another diary can be selected with new data displayed.
+     * after calling this function
+     */
     fun resetState() {
         diaryUiState = DiaryUiState()
     }
 
 }
 
+/**
+ * This holds the diary information that the user inputted or selected, which can then be used to
+ * add a diary, display the diary information to the screen, or even update the diary.
+ */
 data class DiaryUiState(
     val diaryId: String = "",
     val title: String = "",
